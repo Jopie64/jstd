0d50115a6b4d555c50d87100b4dfce73829e9a6d
Johan 't Hart <johanthart@gmail.com>
2010-12-28 15:47:24 +0100

    DirIterator added

---
+++ DirIterator.cpp
@@ -0,0 +1,44 @@
+#include "StdAfx.h"
+#include "DirIterator.h"
+#include "JStd.h"
+#include <windows.h>
+
+
+namespace JStd
+{
+
+CDirIterator::CDirIterator(const wchar_t* dir)
+:	m_Handle(NULL),
+	m_bEnd(true)
+{
+	m_Handle = _wfindfirst64(dir, &m_FindData);
+	if(m_Handle == -1)
+	{
+//		if(errno == ENOENT)
+			return; //No entries. At end. Dont throw.
+		throw std::runtime_error(JStd::String::Format("Cannot scan directory '%s': %d", JStd::String::ToMult(dir, CP_UTF8).c_str(), errno));
+	}
+	m_bEnd = false;
+}
+
+CDirIterator& CDirIterator::operator++()
+{
+	if(_wfindnext64(m_Handle, &m_FindData) == -1)
+		m_bEnd = true;
+	return *this;
+}
+
+CDirIterator::~CDirIterator(void)
+{
+	if(m_Handle != -1)
+		_findclose(m_Handle);
+}
+
+bool CDirIterator::IsDirectory() const
+{
+	return File().attrib & _A_SUBDIR;
+}
+
+
+
+}
---
+++ DirIterator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <io.h>
+
+namespace JStd
+{
+
+class CDirIterator
+{
+public:
+	CDirIterator(const wchar_t* dir);
+	virtual ~CDirIterator(void);
+
+	operator bool()const{return !m_bEnd;}
+
+	CDirIterator& operator++();
+
+	const _wfinddata64_t&	File() const {return m_FindData;}
+
+	bool					IsDirectory() const;
+
+
+private:
+
+	intptr_t		m_Handle;
+	_wfinddata64_t	m_FindData;
+	bool			m_bEnd;
+};
+
+
+}
