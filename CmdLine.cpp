b73075149a8069b8ccd10adaf5bf2d0587d757ff
Johan 't Hart <johanthart@gmail.com>
2011-11-24 23:48:52 +0100

    CmdLine: Now encapsulates argc and argv in CmdLine class

--- CmdLine.cpp
+++ CmdLine.cpp
@@ -10,6 +10,97 @@ namespace CmdLine
 
 using namespace std;
 
+CmdLine::Arg::Arg()
+:	m_isOption(false),
+	m_isRead(false)
+{
+}
+
+CmdLine::CmdLine(int argc, wchar_t* argv[])
+:	m_argc(argc), m_argv(argv)
+{
+	m_vectArgs.reserve(argc);
+	for(int i = 0; i < argc; ++i)
+	{
+		Arg* curr = &*m_vectArgs.insert(m_vectArgs.end(), Arg());
+		const wchar_t* currArg = argv[i];
+		if(*currArg == '-')
+		{
+			curr->m_isOption = true;
+			++currArg;
+			if(*currArg == '-')
+			{
+				++currArg;
+				curr->m_value = currArg;
+				continue;
+			}
+			while(*currArg != '\0')
+			{
+				if(!curr)
+				{
+					curr = &*m_vectArgs.insert(m_vectArgs.end(), Arg());
+					curr->m_isOption = true;
+				}
+
+				curr->m_value.push_back(*currArg);
+				curr = NULL;
+				++currArg;
+			}
+			continue;
+		}
+		curr->m_value = currArg;
+	}
+}
+
+std::wstring CmdLine::next(const wchar_t* longOption)
+{
+	return next(0, longOption);
+}
+
+std::wstring CmdLine::next(wchar_t shortOption, const wchar_t* longOption)
+{
+	VectorArg::iterator i = toOption(shortOption, longOption);
+	if(i == m_vectArgs.end())
+		return L"";
+	if(!shortOption && !longOption)
+		return i->m_value; //Since this is a value without option, i points at the value already.
+
+	++i;
+	if(i == m_vectArgs.end() || i->m_isOption)
+		throw std::runtime_error(String::Format("Expected value after option %s", String::ToMult((--i)->m_value, CP_ACP).c_str()));
+	if(i->m_isRead)
+		throw std::logic_error(String::Format("Value of option %s was already read. This is a software error.", String::ToMult((--i)->m_value, CP_ACP).c_str()));
+	i->m_isRead = true;
+	return i->m_value;
+}
+
+bool CmdLine::hasOption(const wchar_t* longOption)
+{
+	return hasOption(0, longOption);
+}
+
+bool CmdLine::hasOption(wchar_t shortOption, const wchar_t* longOption)
+{
+	return toOption(shortOption, longOption) != m_vectArgs.end();
+}
+
+CmdLine::VectorArg::iterator CmdLine::toOption(wchar_t shortOption, const wchar_t* longOption)
+{
+	for(VectorArg::iterator i = m_vectArgs.begin(); i != m_vectArgs.end(); ++i)
+	{
+		if(i->m_isRead || i->m_value.empty()) continue;
+		if(!longOption && !shortOption && !i->m_isOption) { i->m_isRead = true; return i; }
+		if(!i->m_isOption) continue;
+		if( longOption && i->m_value == longOption ||
+			shortOption && i->m_value[0] == shortOption )
+		{
+			i->m_isRead = true;
+			return i;
+		}
+	}
+	return m_vectArgs.end();
+}
+
 typedef map<wstring, FuncCmd> MapFunc;
 
 struct Registrar: CSingleton<Registrar>
@@ -23,10 +114,10 @@ bool Register(const wchar_t* cmd, const FuncCmd& cmdFunc)
 	return true;
 }
 
-void Call(const wchar_t* cmd, int argc, wchar_t* argv[])
+void Call(const wchar_t* cmd, CmdLine& cmdLine)
 {
 	if (FuncCmd& func = Registrar::I().m_mapCmd[cmd])
-		func(argc, argv);
+		func(std::tr1::ref(cmdLine));
 	else
 		throw std::runtime_error(String::Format("Command '%s' not understood.", String::ToMult(cmd, 0).c_str()));
 
@@ -34,11 +125,14 @@ void Call(const wchar_t* cmd, int argc, wchar_t* argv[])
 
 void CallDefault(int argc, wchar_t* argv[])
 {
-	if(argc < 1)
+	CmdLine cmdLine(argc, argv);
+	if(cmdLine.next().empty())
 		throwUsage(String::Format(L"No options??? Impossible."));
-	if(argc < 2)
+	wstring cmd = cmdLine.next();
+	if(cmd.empty())
 		throwUsage(String::Format(L"Usage: %s [command] <options...>", argv[0]));
-	Call(argv[1], argc, argv);
+
+	Call(cmd.c_str(), cmdLine);
 }
 
 
--- CmdLine.h
+++ CmdLine.h
@@ -1,18 +1,50 @@
 #pragma once
 #include <functional>
+#include <vector>
+#include <string>
 
 namespace JStd
 {
 namespace CmdLine
 {
 
-typedef std::tr1::function<void(int argc, wchar_t* argv[])> FuncCmd;
+class CmdLine
+{
+public:
+	class Arg
+	{
+	public:
+		Arg();
+		bool			m_isOption;
+		std::wstring	m_value;
+		bool			m_isRead;
+	};
+
+	typedef std::vector<Arg> VectorArg;
+
+	CmdLine(int argc, wchar_t* argv[]);
+
+	std::wstring	next(const wchar_t* longOption = NULL);
+	std::wstring	next(wchar_t shortOption, const wchar_t* longOption = NULL);
+
+	bool			hasOption(const wchar_t* longOption = NULL);
+	bool			hasOption(wchar_t shortOption, const wchar_t* longOption = NULL);
+
+private:
+	VectorArg::iterator toOption(wchar_t shortOption, const wchar_t* longOption);
+	int			m_argc;
+	wchar_t**	m_argv;
+	VectorArg	m_vectArgs;
+};
+
+//typedef std::tr1::function<void(int argc, wchar_t* argv[])> FuncCmd;
+typedef std::tr1::function<void(CmdLine&)>					FuncCmd;
 
 //Returns bool so you can do something like
 //static bool registered = CmdLine::Register("command", &onCommand);
 bool Register(const wchar_t* cmd, const FuncCmd& cmdFunc);
 
-void Call(const wchar_t* cmd, int argc, wchar_t* argv[]);
+void Call(const wchar_t* cmd, CmdLine& cmdLine);
 void CallDefault(int argc, wchar_t* argv[]);
 
 class CUsageException
